03e40dd1d3896008f2a1d11f4cd24c8a9af5228b
javedlti <javed.ahmad@exxonmobil.com>
2022-10-24 22:21:21 +0530

    Utopian Tree Problem

---
+++ LeetCodeSolutions/UtopianTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class UtopianTree
+    {
+        public int LenthOfUtopianTree(int growthCycle)
+        {
+            int LengthofTrree=1;
+            for(int i=1;i<=growthCycle;i++)
+            {
+                if(i==0||i%2==0)
+                {
+                    LengthofTrree += 1;
+                }
+                else
+                {
+                    LengthofTrree *= 2;
+                }
+            }
+            return LengthofTrree;
+
+        }
+
+    }
+}
