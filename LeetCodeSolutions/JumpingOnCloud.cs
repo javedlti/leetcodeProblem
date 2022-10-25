372f5caa06b333331533a06fc516fdcd4eef2ed2
javedlti <javed.ahmad@exxonmobil.com>
2022-10-25 14:09:40 +0530

    JumpingOnCloud Revisited

---
+++ LeetCodeSolutions/JumpingOnCloud.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class JumpingOnCloud
+    {
+        public int JumingOnCloud(int[] c, int k)
+        {
+            int[] cloud = c;
+            int jumpSize = k;
+            int energy = 100;
+            int position = 0;
+            int completed = 0;
+            do
+            {
+                position = position + jumpSize;
+
+                if (position >= cloud.Length)
+                {
+                    position = position - cloud.Length;
+                }
+
+                energy = EnegryCalculation(cloud, position, energy);
+                if(position==0)
+                {
+                    completed = 1;
+                }
+            }
+            while (energy > 0 && position <= cloud.Length && completed==0);
+
+            return energy;
+        }
+
+        private int EnegryCalculation(int[] cloud, int position, int energy)
+        {
+            if (cloud[position] == 1)
+            {
+                energy = energy-1 - 2;
+            }
+            else
+            {
+                energy = energy - 1;
+            }
+            return energy;
+        }
+    }
+}
--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -14,9 +14,14 @@ namespace LeetCodeSolutions
             //var output = hR_Repeated.CountACharRepeatedString("ababc", 100);
             //Console.WriteLine(output);
 
-            UtopianTree ut = new UtopianTree();
-            var length= ut.LenthOfUtopianTree(3);
-            Console.WriteLine(length);
+            //UtopianTree ut = new UtopianTree();
+            //var length= ut.LenthOfUtopianTree(3);
+            //Console.WriteLine(length);
+
+            JumpingOnCloud jump = new JumpingOnCloud();
+            int[] arr = new int[] { 0, 0, 1, 0 };
+           var result= jump.JumingOnCloud(new[] { 1,1,1,0,1,1,0,0,0,0 }, 3);
+            Console.WriteLine(result);
             Console.ReadLine();
         }
     }
